b8ac8386bdaa1e9730efbf5e3634dca80041f518
Wei Ling <FooW88@gmail.com>
2022-11-03 00:30:02 +0800

    added queue parameter to merge join algo

--- algos/merge_join.py
+++ algos/merge_join.py
@@ -7,7 +7,7 @@ def merge_join(plan, isStart):
 
     if "Plans" in plan:
         for child in plan["Plans"]:
-            output = output + annotation.processPlan(child, isStart) + " "
+            output = output + annotation.processPlan(child, queue, isStart) + " "
             if isStart:
                 isStart = False
 
