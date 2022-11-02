4f2acd29bcb71781df5bf9bccc0d9a8154b17e15
Wei Ling <FooW88@gmail.com>
2022-11-02 21:48:04 +0800

    sort algorithm done

--- annotation.py
+++ annotation.py
@@ -7,6 +7,7 @@ import algos.generic as generic_algo
 import algos.nested_loop as nested_loop
 import algos.sequential_scan as sequential_scan
 import algos.index_scan as index_scan
+import algos.sort as sort
 
 # PlanTraverser class
 class PlanTraverser:
@@ -17,6 +18,7 @@ class PlanTraverser:
         self.Nested_Loop = nested_loop.nested_loop
         self.Seq_Scan = sequential_scan.sequential_scan
         self.Index_Scan = index_scan.index_scan
+        self.Sort = sort.sort
 
 # Function to process a plan (which is in json format)
 def processPlan(plan, isStart=False):
