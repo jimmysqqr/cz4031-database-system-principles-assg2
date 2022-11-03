acdd5b91f238ac374532c40d0dd9d38796a22deb
Wei Ling <FooW88@gmail.com>
2022-11-03 19:56:56 +0800

    dictionary and queue para removed

--- algos/setop.py
+++ algos/setop.py
@@ -3,16 +3,7 @@
 import annotation
 
 def setop(plan, isStart):
-    # Dict to add
-    q_item = {}
-    q_item["Mode Type"] = plan["Node Type"]
-    q_item["Relation Name"] = plan["Relation Name"]
-    q_item["Total Cost"] = plan["Total Cost"]
-
-    queue.append(q_item)
-
-
-    output = annotation.processPlan(plan["Plans"][0], queue, isStart)
+    output = annotation.processPlan(plan["Plans"][0], isStart)
     output = output + " " + annotation.getConnector()
     output = output + "Detect "
     command_name = str(plan["Command"])
